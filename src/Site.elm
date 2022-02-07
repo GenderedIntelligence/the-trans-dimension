65199d04365b9291d14570a1e889b39a9503927b
katjam <katjamordaunt@gmail.com>
2022-02-07 14:54:13 +0000

    Add elm-pages boilderplate (#12)
    * Add elm-pages boilderplate
    
    * Add package.lock
    
    * Add format command

---
+++ src/Site.elm
@@ -0,0 +1,41 @@
+module Site exposing (config)
+
+import DataSource
+import Head
+import Pages.Manifest as Manifest
+import Route
+import SiteConfig exposing (SiteConfig)
+
+
+type alias Data =
+    ()
+
+
+config : SiteConfig Data
+config =
+    { data = data
+    , canonicalUrl = "http://transdimension.uk/"
+    , manifest = manifest
+    , head = head
+    }
+
+
+data : DataSource.DataSource Data
+data =
+    DataSource.succeed ()
+
+
+head : Data -> List Head.Tag
+head static =
+    [ Head.sitemapLink "/sitemap.xml"
+    ]
+
+
+manifest : Data -> Manifest.Config
+manifest static =
+    Manifest.init
+        { name = "The Trans Dimension"
+        , description = "An online community hub which will connect trans communities across the UK by collating news, events and services by and for trans people in one easy-to-reach place."
+        , startUrl = Route.Index |> Route.toPath
+        , icons = []
+        }
