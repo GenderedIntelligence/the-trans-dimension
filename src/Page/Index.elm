8cd5e48bdbffcefe1a068a8f9666c6f6edc55231
Jasmine Chatfield <jazzchatfield@gmail.com>
2022-02-14 15:06:33 +0000

    Remove todo

--- src/Page/Index.elm
+++ src/Page/Index.elm
@@ -51,7 +51,7 @@ head static =
             }
         , description = "An online community hub which will connect trans communities across the UK by collating news, events and services by and for trans people in one easy-to-reach place. A collaboration between Gendered Intelligence and Geeks for Social Change."
         , locale = Nothing
-        , title = "The Trans Dimension" -- metadata.title -- TODO
+        , title = "The Trans Dimension" -- metadata.title
         }
         |> Seo.website
 
