40ff4d16bb75a09f17ed389c2684f20439df1569
Jasmine Chatfield <jazzchatfield@gmail.com>
2022-02-17 12:00:40 +0000

    Bit of CSS on the header

--- src/PageHeader.elm
+++ src/PageHeader.elm
@@ -2,14 +2,19 @@ module PageHeader exposing (viewPageHeader)
 
 import Copy.Keys exposing (Key(..))
 import Copy.Text exposing (t)
-import Css exposing (Style, batch, color, fontSize, hex, rem)
+import Theme exposing (pink)
+import Css exposing (Style, batch, color, fontSize, hex, rem, displayFlex, justifyContent, width, pct, marginRight, display, backgroundColor, textDecoration, none, center, textAlign)
 import Html.Styled exposing (Html, a, div, h1, header, li, nav, p, text, ul)
 import Html.Styled.Attributes exposing (css, href)
+import Css exposing (spaceBetween)
+import Css exposing (padding)
+import Css exposing (block)
+import Css.Media exposing (grid)
 
 
 viewPageHeader : Html msg
 viewPageHeader =
-    header []
+    header [ css [ headerStyle ]]
         [ viewPageHeaderNavigation ""
         , viewPageHeaderAsk (t SiteHeaderAskButton) "http://donate.com"
         , viewPageHeaderTitle (t SiteTitle) (t SiteStrapline)
@@ -18,7 +23,7 @@ viewPageHeader =
 
 viewPageHeaderTitle : String -> String -> Html msg
 viewPageHeaderTitle pageTitle strapLine =
-    div []
+    div [ ]
         [ h1 [ css [ titleStyle ] ] [ text pageTitle ]
         , p [] [ text strapLine ]
         ]
@@ -27,10 +32,10 @@ viewPageHeaderTitle pageTitle strapLine =
 viewPageHeaderNavigation : String -> Html msg
 viewPageHeaderNavigation listItems =
     nav []
-        [ ul []
-            [ li [] [ text "Item 1" ]
-            , li [] [ text "Item 2" ]
-            , li [] [ text "Item 3" ]
+        [ ul [ css [ navigationListStyle ]]
+            [ li [ css [ navigationListItemStyle ]] [ text "Item 1" ]
+            , li [ css [ navigationListItemStyle ]] [ text "Item 2" ]
+            , li [ css [ navigationListItemStyle ]] [ text "Item 3" ]
             ]
         ]
 
@@ -38,13 +43,45 @@ viewPageHeaderNavigation listItems =
 viewPageHeaderAsk : String -> String -> Html msg
 viewPageHeaderAsk copyText linkTo =
     div []
-        [ a [ href linkTo ] [ text copyText ]
+        [ a [ href linkTo, css [ askButtonStyle ] ] [ text copyText ]
         ]
 
+headerStyle : Style
+headerStyle =
+    batch
+        [
+            padding (rem 1)
+            , textAlign center
+        ]
 
 titleStyle : Style
 titleStyle =
     batch
         [ fontSize (rem 2)
         , color (hex "000000")
+        ]
+
+navigationListStyle : Style
+navigationListStyle =
+    batch
+        [ displayFlex
+        , fontSize (rem 1)
+        ]
+
+navigationListItemStyle : Style
+navigationListItemStyle =
+    batch
+        [ marginRight (rem 1)
+        ]
+
+askButtonStyle : Style
+askButtonStyle =
+    batch
+        [ display block
+        , backgroundColor pink
+        , color (hex "000000")
+        , textDecoration none
+        , padding (rem 1)
+        , width (rem 5)
+        , textAlign center
         ]
