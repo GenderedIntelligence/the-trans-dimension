b649a24928fc154aba1dfbb3df477702329ff409
katjam <katjamordaunt@gmail.com>
2022-02-22 14:57:39 +0000

    Add about page stubs. WIP- content not found error

---
+++ content/about.md
@@ -0,0 +1,12 @@
+---
+title: [cCc] About
+---
+About page content [cCc]
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fames ac turpis egestas sed tempus urna. Arcu cursus vitae congue mauris rhoncus aenean vel. Sed vulputate odio ut enim. 
+
+- Malesuada fames ac turpis egestas sed tempus urna et.
+- Massa eget egestas purus viverra accumsan in. 
+- Fringilla urna porttitor rhoncus dolor purus non.
+
+Sagittis eu volutpat odio facilisis mauris sit amet massa vitae. Nascetur ridiculus mus mauris vitae ultricies leo. Lacus sed viverra tellus in hac habitasse. Dictum varius duis at consectetur lorem donec massa sapien faucibus. Amet luctus venenatis lectus magna fringilla urna.
---
+++ src/Page/About.elm
@@ -0,0 +1,84 @@
+module Page.About exposing (Data, Model, Msg, page)
+
+import DataSource exposing (DataSource)
+import DataSource.File
+import Head
+import Head.Seo as Seo
+import Html.Styled as Html
+import OptimizedDecoder as Decode exposing (Decoder)
+import Page exposing (Page, PageWithState, StaticPayload)
+import Pages.PageUrl exposing (PageUrl)
+import Pages.Url
+import Shared
+import View exposing (View)
+
+
+type alias Model =
+    ()
+
+
+type alias Msg =
+    Never
+
+
+type alias RouteParams =
+    {}
+
+
+page : Page RouteParams Data
+page =
+    Page.single
+        { head = head
+        , data = data
+        }
+        |> Page.buildNoState { view = view }
+
+
+type alias Data =
+    { title : String
+    , body : String
+    }
+
+
+data : DataSource Data
+data =
+    DataSource.File.bodyWithFrontmatter
+        aboutPageDecoder
+        "content/about.md"
+
+
+aboutPageDecoder : String -> Decoder Data
+aboutPageDecoder body =
+    Decode.map (Data body)
+        (Decode.field "title" Decode.string)
+
+
+head :
+    StaticPayload Data RouteParams
+    -> List Head.Tag
+head static =
+    Seo.summary
+        { canonicalUrlOverride = Nothing
+        , siteName = "elm-pages"
+        , image =
+            { url = Pages.Url.external "TODO"
+            , alt = "elm-pages logo"
+            , dimensions = Nothing
+            , mimeType = Nothing
+            }
+        , description = "TODO"
+        , locale = Nothing
+        , title = "TODO title" -- metadata.title -- TODO
+        }
+        |> Seo.website
+
+
+view :
+    Maybe PageUrl
+    -> Shared.Model
+    -> StaticPayload Data RouteParams
+    -> View Msg
+view maybeUrl sharedModel static =
+    { title = static.data.title
+    , body = [ Html.text static.data.body ]
+    }
