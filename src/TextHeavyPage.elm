8845b709a2076f45814056078e8b9e70c8ff1f08
Jasmine Chatfield <jazzchatfield@gmail.com>
2022-02-24 14:15:00 +0000

    Demo page Privacy and basic reusable template

--- src/TextHeavyPage.elm
+++ src/TextHeavyPage.elm
@@ -1,13 +1,22 @@
-module TextHeavyPage exposing (..)
+module TextHeavyPage exposing (view)
 
+import Css exposing (Style, batch, marginBottom, rem)
 import Html.Styled exposing (Html, div, h2, section, text)
+import Html.Styled.Attributes exposing (css)
+import List exposing (concat)
+import Theme
 
 
-view : String -> String -> Html msg
+view : String -> List (Html msg) -> Html msg
 view title body =
     div []
         [ section []
-            [ h2 [] [ text title ]
+            [ h2 [ css [ Theme.pageHeadingStyle ] ] [ text title ]
             ]
-        , section [] [ text body ]
+        , section [ css [ bodyStyle ] ] body
         ]
+
+
+bodyStyle : Style
+bodyStyle =
+    batch [ marginBottom (rem 2) ]
