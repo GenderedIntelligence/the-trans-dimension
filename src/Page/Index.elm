b57bfac5515a249a907883f3a564b69c7b3f356e
Jazz <41483362+jasminechatfield@users.noreply.github.com>
2022-02-25 10:16:47 +0000

    Homepage stub (#75)
    * Start fleshing out site homepage
    
    * Move copy into copy folder
    
    * Move TransDim code out of elm-pages root (#76)
    
    * Move TransDim code out of elm-pages root
    
    * Supply fixtures to tests following move
    
    * Add basic styling
    
    Co-authored-by: katjam <katjamordaunt@gmail.com>

--- src/Page/Index.elm
+++ src/Page/Index.elm
@@ -2,13 +2,17 @@ module Page.Index exposing (Data, Model, Msg, page)
 
 import Copy.Keys exposing (Key(..))
 import Copy.Text exposing (t)
+import Css exposing (Style, auto, backgroundColor, batch, center, color, display, fontSize, inlineBlock, margin, margin2, none, nthOfType, padding, padding2, pct, rem, textAlign, textDecoration, width)
 import DataSource exposing (DataSource)
 import Head
 import Head.Seo as Seo
+import Html.Styled as Html exposing (Html, a, article, h2, li, p, section, text, ul)
+import Html.Styled.Attributes exposing (css, href)
 import Page exposing (Page, StaticPayload)
 import Pages.PageUrl exposing (PageUrl)
 import Pages.Url
 import Shared
+import Theme.Global exposing (..)
 import View exposing (View)
 
 
@@ -68,4 +72,88 @@ view :
     -> StaticPayload Data RouteParams
     -> View Msg
 view maybeUrl sharedModel static =
-    View.placeholder "The Trans Dimension"
+    { title = t SiteTitle
+    , body =
+        [ viewIntro (t IndexIntroMessage) (t IndexIntroButtonText)
+        , viewResources (t IndexResourcesHeading) (t IndexResourcesDescription) (t IndexResourcesButtonText)
+        , viewFeatured (t IndexFeaturedHeader) (t IndexFeaturedButtonText)
+        , viewLatestNews (t IndexNewsHeader) (t IndexNewsButtonText)
+        ]
+    }
+
+
+viewIntro : String -> String -> Html msg
+viewIntro introMsg eventButtonText =
+    section [ css [ sectionStyle ] ]
+        [ p [ css [ introMessageStyle ] ] [ text introMsg ]
+        , a [ href "/events", css [ buttonStyle ] ] [ text eventButtonText ]
+        ]
+
+
+viewResources : String -> String -> String -> Html msg
+viewResources title description buttonText =
+    section [ css [ sectionStyle ] ]
+        [ h2 [ css [ sectionHeaderStyle ] ] [ text title ]
+        , p [] [ text description ]
+        , a [ href "/resources", css [ buttonStyle ] ] [ text buttonText ]
+        ]
+
+
+viewFeatured : String -> String -> Html msg
+viewFeatured title buttonText =
+    section [ css [ sectionStyle ] ]
+        [ h2 [ css [ sectionHeaderStyle ] ] [ text title ]
+        , ul []
+            [ li [] [ text "Featured event [fFf]" ]
+            , li [] [ text "Featured event [fFf]" ]
+            , li [] [ text "Featured event [fFf]" ]
+            ]
+        , a [ href "/events", css [ buttonStyle ] ] [ text buttonText ]
+        ]
+
+
+viewLatestNews : String -> String -> Html msg
+viewLatestNews title buttonText =
+    section [ css [ sectionStyle ] ]
+        [ h2 [ css [ sectionHeaderStyle ] ] [ text title ]
+        , article [] [ text "News item title [fFf]" ]
+        , a [ href "/news", css [ buttonStyle ] ] [ text buttonText ]
+        ]
+
+
+sectionStyle : Style
+sectionStyle =
+    batch
+        [ textAlign center
+        , nthOfType "odd" [ batch [ backgroundColor pink ] ]
+        , nthOfType "even" [ batch [ backgroundColor blue ] ]
+        , padding (rem 1)
+        , width (pct 60)
+        , margin2 (rem 2) auto
+        ]
+
+
+sectionHeaderStyle : Style
+sectionHeaderStyle =
+    batch
+        [ color darkBlue
+        , fontSize (rem 2)
+        ]
+
+
+introMessageStyle : Style
+introMessageStyle =
+    batch
+        [ fontSize (rem 1.5) ]
+
+
+buttonStyle : Style
+buttonStyle =
+    batch
+        [ backgroundColor darkBlue
+        , color white
+        , textDecoration none
+        , padding2 (rem 1) (rem 2)
+        , margin (rem 1)
+        , display inlineBlock
+        ]
