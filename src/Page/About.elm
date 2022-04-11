f598ef346c9be2f0cca8706d9d2266b19afb2310
Jasmine Chatfield <jazzchatfield@gmail.com>
2022-04-11 08:38:37 +0100

    Add missing type and format

--- src/Page/About.elm
+++ src/Page/About.elm
@@ -5,8 +5,8 @@ import DataSource exposing (DataSource)
 import DataSource.File
 import Head
 import Head.Seo as Seo
-import Html.Styled as Html exposing (Html, div, h2, h3, hr, section, text, a)
-import Html.Styled.Attributes exposing (css, href)
+import Html.Styled as Html exposing (Html, a, div, h2, h3, h4, hr, img, section, text)
+import Html.Styled.Attributes exposing (css, href, src, target)
 import List exposing (concat)
 import OptimizedDecoder as Decode
 import Page exposing (Page, PageWithState, StaticPayload)
@@ -14,14 +14,9 @@ import Pages.PageUrl exposing (PageUrl)
 import Pages.Url
 import Shared
 import Theme.Global
+import Theme.PageTemplate as PageTemplate
 import Theme.TransMarkdown as TransMarkdown
 import View exposing (View)
-import Theme.PageTemplate as PageTemplate
-import Html.Styled exposing (h4)
-import Html.Styled exposing (img)
-import Html.Styled.Attributes exposing (src)
-
-import Html.Styled.Attributes exposing (target)
 
 
 type alias Model =
@@ -47,27 +42,29 @@ page =
 
 type alias Data =
     { main : SectionWithTextHeader
-        
     , accessibility : SectionWithTextHeader
-    , makers : List Maker 
+    , makers : List Maker
     , placecal : SectionWithImageHeader
     }
 
+
 type alias SectionWithTextHeader =
     { title : String
-        , subtitle : String
-        , body : List (Html.Html Msg)
-        }
+    , subtitle : String
+    , body : List (Html.Html Msg)
+    }
+
 
 type alias SectionWithImageHeader =
     { title : String
     , subtitleimgalt : String
-        , subtitleimg : String
-        , body : List (Html.Html Msg)
-        }
+    , subtitleimg : String
+    , body : List (Html.Html Msg)
+    }
+
 
 type alias Maker =
-    { name : String, url : String, logo : String , body : List (Html.Html Msg)}
+    { name : String, url : String, logo : String, body : List (Html.Html Msg) }
 
 
 data : DataSource Data
@@ -79,7 +76,7 @@ data =
             , makers = makers
             , placecal = placecal
             }
-        ) 
+        )
         (DataSource.File.bodyWithFrontmatter
             (\markdownString ->
                 Decode.map3
@@ -95,8 +92,10 @@ data =
                         |> TransMarkdown.markdownToView
                         |> Decode.fromResult
                     )
-            ) "content/about/main.md" )
-        ( DataSource.File.bodyWithFrontmatter
+            )
+            "content/about/main.md"
+        )
+        (DataSource.File.bodyWithFrontmatter
             (\markdownString ->
                 Decode.map3
                     (\title subtitle renderedMarkdown ->
@@ -111,11 +110,13 @@ data =
                         |> TransMarkdown.markdownToView
                         |> Decode.fromResult
                     )
-            ) "content/about/accessibility.md" )
-        ( DataSource.map2 
-            (\ gi gfsc ->
-                [ gi, gfsc]
-            )    
+            )
+            "content/about/accessibility.md"
+        )
+        (DataSource.map2
+            (\gi gfsc ->
+                [ gi, gfsc ]
+            )
             (DataSource.File.bodyWithFrontmatter
                 (\markdownString ->
                     Decode.map4
@@ -126,33 +127,38 @@ data =
                             , body = renderedMarkdown
                             }
                         )
-                    (Decode.field "name" Decode.string)
-                    (Decode.field "logo" Decode.string)
-                    (Decode.field "url" Decode.string)                 
-                    (markdownString
-                        |> TransMarkdown.markdownToView
-                        |> Decode.fromResult
-                    )
-            ) "content/about/makers/gi.md" )
+                        (Decode.field "name" Decode.string)
+                        (Decode.field "logo" Decode.string)
+                        (Decode.field "url" Decode.string)
+                        (markdownString
+                            |> TransMarkdown.markdownToView
+                            |> Decode.fromResult
+                        )
+                )
+                "content/about/makers/gi.md"
+            )
             (DataSource.File.bodyWithFrontmatter
                 (\markdownString ->
-                Decode.map4
-                    (\name logo url renderedMarkdown ->
-                        { name = name
-                        , logo = logo
-                        , url = url
-                        , body = renderedMarkdown
-                        }
-                    )
-                    (Decode.field "name" Decode.string)
-                    (Decode.field "logo" Decode.string)
-                    (Decode.field "url" Decode.string)      
-                    (markdownString
-                        |> TransMarkdown.markdownToView
-                        |> Decode.fromResult
-                    )
-            ) "content/about/makers/gfsc.md" ) )
-        ( DataSource.File.bodyWithFrontmatter
+                    Decode.map4
+                        (\name logo url renderedMarkdown ->
+                            { name = name
+                            , logo = logo
+                            , url = url
+                            , body = renderedMarkdown
+                            }
+                        )
+                        (Decode.field "name" Decode.string)
+                        (Decode.field "logo" Decode.string)
+                        (Decode.field "url" Decode.string)
+                        (markdownString
+                            |> TransMarkdown.markdownToView
+                            |> Decode.fromResult
+                        )
+                )
+                "content/about/makers/gfsc.md"
+            )
+        )
+        (DataSource.File.bodyWithFrontmatter
             (\markdownString ->
                 Decode.map4
                     (\title subtitleimg subtitleimgalt renderedMarkdown ->
@@ -169,8 +175,9 @@ data =
                         |> TransMarkdown.markdownToView
                         |> Decode.fromResult
                     )
-            ) "content/about/placecal.md" )
-    
+            )
+            "content/about/placecal.md"
+        )
 
 
 head :
@@ -201,22 +208,26 @@ view :
 view maybeUrl sharedModel static =
     { title = static.data.main.title
     , body =
-        [ PageTemplate.view { title = static.data.main.title , bigText = static.data.main.subtitle, smallText = [] } (Just (section []  static.data.main.body)) (Just (div [] (viewAbout static))) ]
-        
+        [ PageTemplate.view { title = static.data.main.title, bigText = static.data.main.subtitle, smallText = [] } (Just (section [] static.data.main.body)) (Just (div [] (viewAbout static))) ]
     }
 
 
 viewAbout : StaticPayload Data RouteParams -> List (Html.Html Msg)
 viewAbout static =
-    [ section [] [ h3 [] [ text static.data.accessibility.title ]
-                 , h4 [] [ text static.data.accessibility.subtitle ]
-                 , div [] static.data.accessibility.body
-                 ]
-    , section [] (List.map (\maker -> (viewMaker maker)) static.data.makers)
+    [ section []
+        [ h3 [] [ text static.data.accessibility.title ]
+        , h4 [] [ text static.data.accessibility.subtitle ]
+        , div [] static.data.accessibility.body
+        ]
+    , section [] (List.map (\maker -> viewMaker maker) static.data.makers)
     ]
 
+
+viewMaker : Maker -> Html.Html Msg
 viewMaker maker =
-     section [] [ h4 [] [ text maker.name]
-                , img [ src maker.logo ] []
-                , div [] maker.body 
-                , a [ href maker.url, target "_blank"] [ text "Find out more about ", text maker.name]]
+    section []
+        [ h4 [] [ text maker.name ]
+        , img [ src maker.logo ] []
+        , div [] maker.body
+        , a [ href maker.url, target "_blank" ] [ text "Find out more about ", text maker.name ]
+        ]
