120f6fae4cfdf3079be75eb32a24007e532f5f0b
Jasmine Chatfield <jazzchatfield@gmail.com>
2022-04-13 13:00:15 +0100

    Pass tests

--- src/Page/Join.elm
+++ src/Page/Join.elm
@@ -2,7 +2,7 @@ module Page.Join exposing (Data, Model, Msg, page, view)
 
 import Copy.Keys exposing (Key(..))
 import Copy.Text exposing (t)
-import Css exposing (Style, alignItems, backgroundColor, batch, block, borderColor, borderRadius, borderStyle, borderWidth, center, color, column, display, displayFlex, flexDirection, fontWeight, int, justifyContent, letterSpacing, margin, margin2, padding, padding2, property, px, rem, row, solid, textAlign, textTransform, uppercase)
+import Css exposing (Style, alignItems, auto, backgroundColor, batch, block, border, borderBox, borderColor, borderRadius, borderStyle, borderWidth, boxSizing, calc, center, color, column, display, displayFlex, fitContent, flexDirection, flexShrink, flexWrap, fontSize, fontWeight, height, important, int, justifyContent, letterSpacing, margin, margin2, marginRight, marginTop, maxWidth, minus, none, padding, padding2, pct, property, px, rem, row, solid, spaceBetween, textAlign, textTransform, uppercase, width, wrap)
 import DataSource exposing (DataSource)
 import Head
 import Head.Seo as Seo
@@ -12,32 +12,9 @@ import Page exposing (Page, StaticPayload)
 import Pages.PageUrl exposing (PageUrl)
 import Pages.Url
 import Shared
-import Theme.Global
+import Theme.Global exposing (withMediaSmallDesktopUp, withMediaTabletLandscapeUp, withMediaTabletPortraitUp)
 import Theme.PageTemplate as PageTemplate exposing (BigTextType(..), HeaderType(..))
 import View exposing (View)
-import Css exposing (height)
-import Css exposing (border)
-import Css exposing (none)
-import Css exposing (fontSize)
-import Css exposing (flexWrap)
-import Css exposing (wrap)
-import Theme.Global exposing (withMediaTabletPortraitUp)
-import Css exposing (width)
-import Css exposing (pct)
-import Css exposing (boxSizing)
-import Css exposing (borderBox)
-import Css exposing (minus)
-import Css exposing (calc)
-import Css exposing (auto)
-import Css exposing (fitContent)
-import Css exposing (maxWidth)
-import Css exposing (important)
-import Css exposing (marginRight)
-import Css exposing (flexShrink)
-import Css exposing (marginTop)
-import Css exposing (spaceBetween)
-import Theme.Global exposing (withMediaSmallDesktopUp)
-import Theme.Global exposing (withMediaTabletLandscapeUp)
 
 
 type alias Model =
@@ -134,7 +111,7 @@ viewForm =
                 ]
             ]
         , label [ css [ formTextAreaItemStyle ] ] [ span [ css [ formTextAreaLabelStyle ] ] [ text (t JoinFormInputMessageLabel) ], textarea [ placeholder (t JoinFormInputMessagePlaceholder), css [ textAreaStyle ] ] [] ]
-        , div [ css [ buttonWrapperStyle ] ] [ button [ css [ buttonStyle ] , type_ "submit" ] [ text (t JoinFormSubmitButton) ] ]
+        , div [ css [ buttonWrapperStyle ] ] [ button [ css [ buttonStyle ], type_ "submit" ] [ text (t JoinFormSubmitButton) ] ]
         ]
 
 
@@ -142,13 +119,14 @@ formStyle : Style
 formStyle =
     batch
         [ margin2 (rem 2) (rem 0)
-        , withMediaSmallDesktopUp [ maxWidth (px 900), margin2 (rem 2) auto]
-        , withMediaTabletLandscapeUp [ fontSize (rem 1.2)]
+        , withMediaSmallDesktopUp [ maxWidth (px 900), margin2 (rem 2) auto ]
+        , withMediaTabletLandscapeUp [ fontSize (rem 1.2) ]
         , withMediaTabletPortraitUp
             [ displayFlex
             , flexWrap wrap
             , justifyContent spaceBetween
-            ] ]
+            ]
+        ]
 
 
 formItemStyle : Style
@@ -163,12 +141,15 @@ formItemStyle =
             ]
         ]
 
+
 formTextAreaItemStyle : Style
 formTextAreaItemStyle =
     batch
         [ formItemStyle
-        , withMediaTabletPortraitUp [ important (width (pct 100))]
-        , flexDirection row]
+        , withMediaTabletPortraitUp [ important (width (pct 100)) ]
+        , flexDirection row
+        ]
+
 
 formLabelStyle : Style
 formLabelStyle =
@@ -181,6 +162,7 @@ formLabelStyle =
         , letterSpacing (px 1.6)
         ]
 
+
 formTextAreaLabelStyle : Style
 formTextAreaLabelStyle =
     batch
@@ -192,12 +174,14 @@ formTextAreaLabelStyle =
             ]
         ]
 
+
 formCheckboxTitleStyle : Style
 formCheckboxTitleStyle =
     batch
         [ formLabelStyle
         , withMediaTabletPortraitUp [ marginRight (rem 2) ]
         ]
+
 
 formInputStyle : Style
 formInputStyle =
@@ -211,14 +195,18 @@ formInputStyle =
         , color Theme.Global.white
         ]
 
+
 formCheckboxWrapperStyle : Style
 formCheckboxWrapperStyle =
     batch
-        [ withMediaTabletPortraitUp 
+        [ withMediaTabletPortraitUp
             [ width (pct 100)
             , displayFlex
             , justifyContent center
-            , alignItems center]]
+            , alignItems center
+            ]
+        ]
+
 
 formCheckboxItemStyle : Style
 formCheckboxItemStyle =
@@ -261,18 +249,23 @@ textAreaStyle =
         , boxSizing borderBox
         ]
 
+
 buttonWrapperStyle : Style
 buttonWrapperStyle =
     batch
         [ textAlign center
-        , withMediaTabletPortraitUp [ width (pct 100)] ]
+        , withMediaTabletPortraitUp [ width (pct 100) ]
+        ]
+
+
 buttonStyle : Style
 buttonStyle =
     batch
         [ backgroundColor Theme.Global.pink
-        , color Theme.Global.darkBlue 
+        , color Theme.Global.darkBlue
         , borderRadius (rem 0.3)
         , borderStyle none
         , padding2 (rem 0.25) (rem 4)
         , fontSize (rem 1.2)
-        , fontWeight (int 500) ]
+        , fontWeight (int 500)
+        ]
