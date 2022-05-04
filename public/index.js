3f181d419d48a86bf4063ca5f03227835183921e
katjam <katjamordaunt@gmail.com>
2022-05-04 13:17:30 +0100

    Temporarily add hotjar tracking

--- public/index.js
+++ public/index.js
@@ -1,10 +1,18 @@
-/** @typedef {{load: (Promise<unknown>); flags: (unknown)}} ElmPagesInit */
+/** Hotjar tracking script **/
+(function(h,o,t,j,a,r){
+  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+  h._hjSettings={hjid:2953975,hjsv:6};
+  a=o.getElementsByTagName('head')[0];
+  r=o.createElement('script');r.async=1;
+  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 
+/** @typedef {{load: (Promise<unknown>); flags: (unknown)}} ElmPagesInit */
 /** @type ElmPagesInit */
 export default {
   load: async function (elmLoaded) {
     const app = await elmLoaded;
-    console.log("App loaded", app);
   },
   flags: function () {
     return "You can decode this in Shared.elm using Json.Decode.string!";
