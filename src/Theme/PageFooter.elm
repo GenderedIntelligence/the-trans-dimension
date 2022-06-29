70263517a2fd34a173b4764c6fd0a51532624074
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-06-29 16:24:26 +0100

    Center GI logo on phone devices

--- src/Theme/PageFooter.elm
+++ src/Theme/PageFooter.elm
@@ -2,7 +2,7 @@ module Theme.PageFooter exposing (viewPageFooter)
 
 import Copy.Keys exposing (Key(..))
 import Copy.Text exposing (t)
-import Css exposing (Style, active, after, alignItems, auto, backgroundColor, backgroundImage, backgroundSize, batch, block, borderBox, borderColor, borderRadius, borderStyle, borderWidth, boxSizing, center, color, column, display, displayFlex, flexDirection, flexEnd, flexShrink, flexWrap, focus, fontSize, fontWeight, height, hover, int, justifyContent, letterSpacing, lineHeight, margin, margin2, margin4, marginBottom, marginRight, marginTop, maxWidth, none, nthLastChild, outline, padding, padding2, padding4, pct, property, pseudoElement, px, rem, row, solid, spaceAround, spaceBetween, stretch, textAlign, textDecoration, textTransform, uppercase, url, width, wrap, inherit)
+import Css exposing (Style, active, after, alignItems, alignSelf, auto, backgroundColor, backgroundImage, backgroundSize, batch, block, borderBox, borderColor, borderRadius, borderStyle, borderWidth, boxSizing, center, color, column, display, displayFlex, flexDirection, flexEnd, flexShrink, flexWrap, focus, fontSize, fontWeight, height, hover, inherit, int, justifyContent, letterSpacing, lineHeight, margin, margin2, margin4, marginBottom, marginRight, marginTop, maxWidth, none, nthLastChild, outline, padding, padding2, padding4, pct, property, pseudoElement, px, rem, row, solid, spaceAround, spaceBetween, stretch, textAlign, textDecoration, textTransform, uppercase, url, width, wrap)
 import Css.Transitions exposing (transition)
 import Helpers.TransRoutes as TransRoutes exposing (Route(..))
 import Html.Styled exposing (Html, a, button, div, footer, form, img, input, label, li, nav, p, span, text, ul)
@@ -373,6 +373,7 @@ logoGIStyle =
         , hover [ backgroundImage (url "/images/logos/GI_pink_rollover.png") ]
         , focus [ backgroundImage (url "/images/logos/GI_white.png") ]
         , active [ backgroundImage (url "/images/logos/GI_white.png") ]
+        , alignSelf center
         ]
 
 
@@ -400,7 +401,6 @@ formStyle =
         , boxSizing borderBox
         , color white
         , marginBottom (rem 1)
-        
         ]
 
 
@@ -429,20 +429,20 @@ formInputStyle =
         , withMediaTabletPortraitUp [ margin4 (rem 0) (rem 1) (rem 0) (rem 0) ]
         ]
 
+
 signupButtonStyle : Style
 signupButtonStyle =
     batch
-        [
-        padding2 (rem 0.25) (rem 1.25)
+        [ padding2 (rem 0.25) (rem 1.25)
         , maxWidth none
         , width (pct 100)
-        , withMediaTabletPortraitUp [
-            -- backgroundColor (hex "f0f")
-            flexShrink (int 0)
+        , withMediaTabletPortraitUp
+            [ flexShrink (int 0)
             , maxWidth inherit
             , width auto
             ]
         ]
+
 
 infoParagraphStyle : Style
 infoParagraphStyle =
