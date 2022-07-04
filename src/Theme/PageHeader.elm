ab7995ef7341d8951615307b9e17884d33237e06
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-07-04 11:34:41 +0100

    Code cleanup

--- src/Theme/PageHeader.elm
+++ src/Theme/PageHeader.elm
@@ -2,7 +2,7 @@ module Theme.PageHeader exposing (viewPageHeader)
 
 import Copy.Keys exposing (Key(..))
 import Copy.Text exposing (t)
-import Css exposing (Style, alignItems, backgroundColor, batch, block, border, borderBottomColor, borderBottomStyle, borderBottomWidth, borderBox, boxSizing, center, color, column, columnReverse, cursor, display, displayFlex, flexDirection, flexGrow, flexWrap, fontSize, fontWeight, hover, int, justifyContent, lighter, margin, margin2, marginRight, none, padding, padding2, padding4, paddingBottom, paddingLeft, pointer, rem, row, solid, spaceBetween, textAlign, textDecoration, transparent, unset, wrap, zero)
+import Css exposing (Style, alignItems, alignSelf, auto, backgroundColor, batch, block, border, borderBottomColor, borderBottomStyle, borderBottomWidth, borderBox, borderRadius, boxSizing, center, color, column, columnReverse, cursor, display, displayFlex, flexDirection, flexGrow, flexWrap, fontSize, fontWeight, hover, int, justifyContent, lighter, margin, margin2, marginLeft, marginRight, none, padding, padding2, padding4, paddingBottom, paddingLeft, pointer, rem, row, solid, spaceBetween, textAlign, textDecoration, transparent, unset, wrap, zero)
 import Css.Transitions exposing (transition)
 import Helpers.TransRoutes as TransRoutes exposing (..)
 import Html.Styled exposing (Html, a, button, div, h1, header, li, nav, span, text, ul)
@@ -111,7 +111,8 @@ viewPageHeaderAsk copyText linkTo =
         [ a
             [ href linkTo
             , css
-                [ navigationLinkStyle, askButtonStyle
+                [ navigationLinkStyle
+                , askButtonStyle
 
                 {- Theme.whiteButtonStyle -}
                 ]
@@ -164,10 +165,7 @@ barStyle : Style
 barStyle =
     batch
         [ withMediaTabletPortraitUp
-            [ -- displayFlex
-              --, justifyContent spaceBetween
-              --, padding (rem 0.5)
-              backgroundColor pink
+            [ backgroundColor pink
             , alignItems center
             ]
         ]
@@ -239,7 +237,7 @@ navigationListItemStyle =
         , fontSize (rem 1.2)
         , withMediaTabletPortraitUp
             [ padding2 (rem 1) (rem 0.75)
-            , Css.alignSelf center
+            , alignSelf center
             ]
         ]
 
@@ -298,7 +296,7 @@ askStyle =
     batch
         [ padding (rem 0)
         , withMediaTabletPortraitUp
-            [ Css.marginLeft Css.auto
+            [ marginLeft auto
             , display unset
             ]
         ]
@@ -312,8 +310,7 @@ askButtonStyle =
         , padding (rem 1)
         , hover [ color pink ]
         , withMediaTabletPortraitUp
-            --  [ display unset
             [ padding4 (rem 0.375) (rem 1.25) (rem 0.5) (rem 1.25)
-            , Css.borderRadius (rem 0.3)
+            , borderRadius (rem 0.3)
             ]
         ]
